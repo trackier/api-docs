15e667699b14030b5cee0496fa95810053781941
Arshal Soren <arshal@trackier.com>
2023-11-01 11:29:36 +0530

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNDkwNQ/spectral.js?branch_slug=master&token=238be5ed-d334-4fcc-b2a5-c78cce3d3852";
+export default { extends: ruleset };
