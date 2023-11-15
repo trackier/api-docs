002dc0fdc358a0ac4f09d4ab7569e215989ca9f6
hemantmann <hemant.mann@trackier.com>
2023-11-15 23:54:16 +0530

    fix: errors

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNjMyMA/spectral.js?branch_slug=master&token=8fa2d945-f15f-475b-89ee-dfefb9c03b2d";
+export default { extends: ruleset };
